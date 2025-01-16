1eb0fd1fdb68a7d1628ec4a4413bfada6f9cc489
reqnwn <aristyo42005@gmail.com>
2025-01-17 01:27:40 +0700

    #aris 2

---
+++ app/Http/Requests/Admin/EditPostRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EditPostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title'     => 'required',
+            'image'     => 'nullable|image|dimensions:max_width=2000,max_height=2000',
+            'post'      => 'required',
+            'category'  => 'required|integer|exists:categories,id',
+            'tags'      => 'required'
+        ];
+    }
+}
